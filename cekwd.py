854f3f610b83ebf033438de093e22030968b8449
inforesult <zaqi26680@gmail.com>
2025-10-21 09:41:36 +0700

    Update cekwd.py

--- cekwd.py
+++ cekwd.py
@@ -105,6 +105,7 @@ def cek_saldo_dan_status(playwright, situs, userid, bataswd=""):
         saldo_value = parse_saldo(saldo_text)
 
         page.goto(f"https://{situs}/#/betRecords")
+        time.sleep(3)
         page.get_by_text("Togel").click()
         page.locator(".list .ls-list-item").first.wait_for(timeout=10000)
         nama_permainan = page.locator(".list .ls-list-item").first.locator("li").nth(2).inner_text().strip()
